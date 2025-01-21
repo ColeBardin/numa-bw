e954b50c3ff68a57668e34e566b90b28022f37c7
Cole Bardin <cab572@drexel.edu>
2025-01-21 15:03:23 -0500

    added mem flag and help option

--- src/main.c
+++ src/main.c
@@ -2,21 +2,26 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-#define USAGE fprintf(stderr, "Usage: %s [-n numas] [-c cpn]\n", argv[0])
+#define USAGE fprintf(stderr, "Usage: %s [-h] [-n numas] [-c cpn] [-m mem]\n", argv[0])
 
 int main(int argc, char **argv)
 {
-    int numas;
-    int cpn;
     int flags;
     int opt;
+    int numas = 0;
+    int cpn = 0;
+    int mem = 0;
     
-    numas = 0;
-    cpn = 0;
-    while((opt = getopt(argc, argv, "n:c:")) != -1)
+    while((opt = getopt(argc, argv, "hn:c:m:")) != -1)
     {
         switch(opt)
         {
+            case 'h':
+                USAGE;
+                puts("  -h        help, prints out the menu");
+                puts("  -n numas  number of numa nodes to use. 0/default, use all available nodes");
+                puts("  -c cpn    number of cores per numa node to use. 0/default, will use maximum number of cores per numa node");
+                puts("  -m mem    total amount of memory to allocate in bytes. 0/default, will utilize all available memory");
             case 'n':
                 numas = atoi(optarg);    
                 if(numas < 0)
@@ -35,6 +40,15 @@ int main(int argc, char **argv)
                     return EXIT_FAILURE;
                 }
                 break;
+            case 'm':
+                mem = atoi(optarg);    
+                if(mem < 0)
+                {
+                    fprintf(stderr, "ERROR: failed to parse m flag argument: %s\n", optarg); 
+                    USAGE;
+                    return EXIT_FAILURE;
+                }
+                break;
             default:
                 fprintf(stderr, "ERROR: unrecognized argument: %c\n", opt);
                 USAGE;
@@ -42,6 +56,9 @@ int main(int argc, char **argv)
         }
     }
 
-    printf("numas: %d\ncpn: %d\n", numas, cpn);
+    printf("numas: %d\n", numas);
+    printf("cpn: %d\n", cpn);
+    printf("mem: %d\n", mem);
     return 0;
 }
+
